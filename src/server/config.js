462b7e3492aa719007b4b378f527c7c42e91c91f
JAMES, ROSHAN <kjroshan@gmail.com>
2019-01-14 18:01:29 -0500

    fixed issue with connection with trailer app in docker

--- src/server/config.js
+++ src/server/config.js
@@ -2,6 +2,6 @@ export default {
     movieList: {
         url: 'https://content.viaplay.se/pc-se/film'
     },
-    API_SERVER_ADRRESS: 'http://localhost:3000',
+    API_SERVER_ADRRESS: 'http://apiapp:3000',
     VIDEO_SERVICE_URL: 'https://content.viaplay.se/pc-se/film/'
 };
