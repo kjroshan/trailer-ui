e651162301d28b0a3ce5d22124974f141c25b001
JAMES, ROSHAN <kjroshan@gmail.com>
2019-01-14 19:09:37 -0500

    Environment variable handling changed

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,10 @@
+const _env = process.env;
+const TRAILER_API_HOST = _env.TRAILER_API_HOST || 'localhost';
+
 export default {
     movieList: {
         url: 'https://content.viaplay.se/pc-se/film'
     },
-    API_SERVER_ADRRESS: 'http://apiapp:3000',
+    API_SERVER_ADRRESS: `http://${TRAILER_API_HOST}:3000`,
     VIDEO_SERVICE_URL: 'https://content.viaplay.se/pc-se/film/'
 };
